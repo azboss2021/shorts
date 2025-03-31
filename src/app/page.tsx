1a961315e6db3a650d576a5f6b939701db347545
Caleb Wilson <alexanderzyn1@gmail.com>
2025-03-31 11:42:31 -0700

    Change background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="h-full min-h-screen w-full bg-gradient-to-b from-black to-purple-900 p-8 text-white">
+    <main className="h-full min-h-screen w-full bg-gradient-to-b from-violet-950 to-black p-8 text-white">
       <div className="mx-auto flex max-w-7xl flex-col items-center gap-8"></div>
     </main>
   );
