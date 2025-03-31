12e4df160049c4bde3413c78c6d66db2505554d2
Caleb Wilson <alexanderzyn1@gmail.com>
2025-03-31 11:56:30 -0700

    Accordion done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Accordion from "@/components/Accordion";
+
 export default function Home() {
   return (
     <main className="h-full min-h-screen w-full bg-gradient-to-b from-violet-950 to-black p-8 text-white">
-      <div className="mx-auto flex max-w-7xl flex-col items-center gap-8"></div>
+      <div className="mx-auto flex max-w-7xl flex-col items-center gap-8">
+        <Accordion />
+      </div>
     </main>
   );
 }
